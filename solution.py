d18008cbddf354b04f7eb68d05f18165e9965f53
NiripsaKakVsegda <anna.poluyanenko@gmail.com>
2023-04-05 05:13:59 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -10,4 +10,10 @@ def solution(p: float, x: np.array) -> tuple:
     alpha = 1 - p
     loc = x.mean()
     scale = np.sqrt(np.var(x)) / np.sqrt(len(x))
-    return 0.077 / (1 - alpha), max(x)
+    
+    n = len(x)
+    k = int(n * alpha / 2)
+    sorted_levels = sorted(x)
+    b_min = sorted_levels[k]
+    b_max = sorted_levels[n-k-1]
+    return (b_min, b_max)
