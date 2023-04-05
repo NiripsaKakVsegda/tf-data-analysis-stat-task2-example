9904eab44a45c3a52863d963b6a6234925184c44
NiripsaKakVsegda <anna.poluyanenko@gmail.com>
2023-04-05 05:39:28 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -7,13 +7,11 @@ from scipy.stats import norm
 chat_id = 440047378
 
 def solution(p: float, x: np.array) -> tuple:
-    alpha = 1 - p
-    loc = x.mean()
-    scale = np.sqrt(np.var(x)) / np.sqrt(len(x))
-    
     n = len(x)
-    k = int(n * alpha / 2)
-    sorted_levels = sorted(x)
-    b_min = sorted_levels[k]
-    b_max = sorted_levels[n-k-1]
-    return (b_min, b_max)
+    mean = np.mean(x)
+    std = np.std(x, ddof=1) / np.sqrt(n) 
+    alpha = 1 - p
+    t_score = abs(stats.t.ppf(alpha / 2, n - 1))
+    left_bound = mean - t_score * std
+    right_bound = mean + t_score * std
+    return left_bound, right_bound
